b4d10d71ab7daa86641fa70564f9dad3dd9c071d
Jacky Liang <asheriffy@gmail.com>
2022-04-04 16:00:56 -0400

    commit tree 0d4df393c20d1072a8f3f7f5876dd1710ddb1fb1

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b){
+return (a+b)
+}
+
+function subtract(a,b){
+return (a-b)
+}
+
+function multiply(a,b){
+return (a*b)
+}
+
+function divide(a,b){
+return (a/b)
+}
+
+function increment(n){
+return (n += 1)
+}
+
+function decrement(n){
+return (n -= 1)
+}
+
+function makeInt(n){
+   const m = parseInt(n, 10);
+    return (m);
+}
+
+function preserveDecimal(n){
+  const m = parseFloat(n);
+    return (m);
+}
